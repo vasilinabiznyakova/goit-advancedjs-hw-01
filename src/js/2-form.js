9c09051bcfed3fd3c353573ab5bab260aa7a2075
Vasylyna Bizniakova <vbiznyakova@gmail.com>
2026-04-13 19:16:00 +0300

    add form

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  if (!(name in formData)) {
+    return;
+  }
+
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email.trim() || !formData.message.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = '';
+  formData.message = '';
+
+  form.reset();
+});
